4e1e336a0d32c5398fe976323fe6b8096c7b83af
sahasra9753 <sahasra.praveenkumar@gmail.com>
2026-01-06 08:48:20 -0700

    almost finished criteria checker

--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/PasswordCheckerApp.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/PasswordCheckerApp.java
@@ -1,29 +1,16 @@
 package org.hillcrest.chapter6.password;
-
+import java.util.Scanner;
 public class PasswordCheckerApp {
     //main method goes here
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        System.out.println("Enter a password: ");
+        String password = scanner.nextLine();
+
+        System.out.println("Password Strength: " + );
    
-    //prompt user for password
-    System.out.print("Enter a password: ");
-    String password = scanner.nextLine();
 
-    int score = CriteriaChecker.evaluateCriteria(password);
-    String strength = CriteriaChecker.determineStrength(score);
-
-
-    System.out.println("Password Strength: " + strength + " (" + score + "/5");
-    //Check strength
-        
-    //Provide user feedback based on the strength
-    if (!feedback.isEmpty()) {
-        System.out.println("Suggestions to improve your password:");
-        System.out.print(feedback);
-        }
-
-    scanner.close();
     }
 }
     
