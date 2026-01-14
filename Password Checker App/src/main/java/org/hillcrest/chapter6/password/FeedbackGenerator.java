c3c969434dbf1b1d46a0fe5783bfd500baa39116
sahasra9753 <sahasra.praveenkumar@gmail.com>
2026-01-13 21:27:24 -0700

    Enhance password feedback generation
    Added additional feedback for password requirements.

--- Password Checker App/src/main/java/org/hillcrest/chapter6/password/FeedbackGenerator.java
+++ Password Checker App/src/main/java/org/hillcrest/chapter6/password/FeedbackGenerator.java
@@ -6,6 +6,15 @@ public class FeedbackGenerator {
     public static String generateFeedback(String password) {
         if (password.length() < 8) {
             return "Increase to at least 8 characters.";
-        }
-    }
+        } else if (!hasUpper) {
+            return "Add at least one uppercase letter";
+        } else if (!hasLower) {
+            return "Add at least one lowercase letter.";
+        } else if (!hasDigit) {
+            return "Add at least one number.";
+        } else if (!hasSpecial) {
+            return "Include a special charecter.";
+        } 
+        return feedback.toString();
 }
+}        
